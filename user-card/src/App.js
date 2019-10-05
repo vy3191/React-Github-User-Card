0a861af55de77a8778a3d75ba03aef81bc4f8d26
vy3191 <venkyafhs@gmail.com>
2019-10-05 09:36:34 -0400

    Started getting data

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import axios from 'axios';
+import {Card, Button } from 'react-bootstrap';
 
 export default class App extends Component {
   constructor() {
@@ -33,7 +34,17 @@ export default class App extends Component {
     return (
       <div>
         <h1 style={{textAlign:'center', color:'red'}}>User-Card-Project</h1>
-
+        <Card style={{ width: '18rem' }}>
+          <Card.Img variant="top" src={this.state.info.avatar_url} />
+          <Card.Body>
+            <Card.Title>{this.state.info.name}</Card.Title>
+            <Card.Text>
+              <p style={{display:'inline-block'}}>Followers:</p><span>{this.state.info.followers}</span>{" "}
+              <p style={{display:'inline-block'}}>Followers:</p><span>{this.state.info.following}</span>
+            </Card.Text>
+            <Button variant="success"><a href={this.state.info.html_url}>Link To My Github</a></Button>
+          </Card.Body>
+        </Card>
       </div>
     )
   }
